62c7a17667f45453664dacfb216fa581d35952ff
Samuel Knight <robogamingbysam@icloud.com>
2025-07-05 03:27:27 -0500

    Add syntax highlighting

--- .eleventy.js
+++ .eleventy.js
@@ -2,9 +2,11 @@ const eleventySass = require("@11tyrocks/eleventy-plugin-sass-lightningcss");
 const { eleventyImageTransformPlugin } = require("@11ty/eleventy-img");
 const markdownItCheckbox = require("markdown-it-task-checkbox");
 const markdownItFootnote = require("markdown-it-footnote");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
-module.exports = (eleventyConfig) => {
+export default (eleventyConfig) => {
   eleventyConfig.addPlugin(eleventySass);
+  eleventyConfig.addPlugin(syntaxHighlight);
 
   // Add image optimization plugin
   eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
